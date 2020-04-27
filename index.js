56bf5f515abe37af62e3d4875a5f4b3ffe3d3e6f
Mehdi Amrane <me.amrane@gmail.com>
2020-04-27 03:29:32 +0200

    Part3/phonebook-api: 3.5 solution added (#6)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     name: 'Arto Hellas',
@@ -53,6 +55,17 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+  const randomId = Math.floor(Math.random() * 1000000);
+
+  const person = request.body;
+  person.id = randomId;
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
