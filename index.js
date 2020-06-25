11fde5be43d244bf968c23dd78c0476e508746ac
Mehdi Amrane <me.amrane@gmail.com>
2020-06-25 17:16:43 +0200

    Part3/phonebook-api: 3.13 solution added (#16)

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Person = require('./models/person');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
-
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1,
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2,
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3,
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4,
-  },
-];
 
 morgan.token('data', (request) => {
   return JSON.stringify(request.body);
@@ -42,32 +22,42 @@ app.get('/', (req, res) => {
 
 app.get('/info', (req, res) => {
   var d = new Date();
-  res.send(`<p>Phonebook has info for ${persons.length} people. <br/> ${d}</p>`);
+  Person.find({}).then((result) => {
+    res.send(`<p>Phonebook has info for ${result.length} people. <br/> ${d}</p>`);
+  });
 });
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person.find({}).then((result) => {
+    res.json(result);
+  });
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete('/api/persons/:id', (request, response) => {
+  let persons = [];
+  Person.find({}).then((result) => {
+    persons = result;
+  });
+
+  // Not working yet
   const id = Number(request.params.id);
   persons = persons.filter((person) => person.id !== id);
-
   response.status(204).end();
 });
 
 app.post('/api/persons', (request, response) => {
+  let persons = [];
+  Person.find({}).then((result) => {
+    persons = result;
+  });
+
+  // Not working yet
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -98,7 +88,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
